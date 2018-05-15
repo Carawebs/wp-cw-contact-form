aec2cb831d14e119aa50b393fb7459428bdbb06a
David Egan <david@carawebs.com>
2018-05-15 10:28:10 +0100

    Tidy up.

--- src/Traits/PartialSelector.php
+++ src/Traits/PartialSelector.php
@@ -7,23 +7,22 @@ namespace Carawebs\ContactForm\Traits;
 */
 trait PartialSelector {
 
-    public function partial_selector( $partial )
+    public function partial_selector($partial)
     {
-        if ( file_exists( get_template_directory() .'/'. $partial . '.php' ) ) {
-            return ( get_template_directory() .'/'. $partial . '.php' );
+        if (file_exists(get_template_directory() .'/'. $partial . '.php')) {
+            return (get_template_directory() .'/'. $partial . '.php');
         } else {
-            return ( dirname(__DIR__) . '/partials/' . $partial . '.php' );
+            return (dirname(__DIR__) . '/partials/' . $partial . '.php');
         }
     }
 
-    public static function static_partial_selector( $partial ) {
+    public static function static_partial_selector($partial) {
         $override = get_template_directory() . '/templates/' . $partial . '.php';
-        // die(var_dump($override));
 
-        if ( file_exists( $override ) ) {
-            return ( $override );
+        if (file_exists($override)) {
+            return ($override);
         } else {
-            return ( dirname(__DIR__) . '/' . $partial . '.php' );
+            return (dirname(__DIR__) . '/' . $partial . '.php');
         }
     }
 }
