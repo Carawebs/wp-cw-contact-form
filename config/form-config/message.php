4126dbf5a080e699cf11ada14a7a027111ec7284
David Egan <david@carawebs.com>
2018-05-15 17:30:15 +0100

    Remove ignored files.

--- config/form-config/message.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) exit;
-$fields = [
-    'email' => '',
-    'email_from' => 'server@getitright.uk.com',
-    'subject' => 'Form submission: Get it Right Initiative.',
-    'header-from' => get_bloginfo('name'),
-];
-return $fields;
